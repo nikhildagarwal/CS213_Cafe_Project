3c543d23b965caf8aecff87c832346b4bd7f29b4
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-03-27 20:39:00 -0400

    coffee comments

--- src/main/java/com/example/cs213_cafe_project/cofee/Size.java
+++ src/main/java/com/example/cs213_cafe_project/cofee/Size.java
@@ -2,15 +2,21 @@ package com.example.cs213_cafe_project.cofee;
 
 public enum Size {
 
-    SHORT(1.89),
-    TALL(2.29),
-    GRANDE(2.69),
-    VENTI(3.09);
+    SHORT(1.89,"Short"),
+    TALL(2.29,"Tall"),
+    GRANDE(2.69,"Grande"),
+    VENTI(3.09,"Venti");
 
     private double price;
+    private String size;
 
-    Size(double price){
+    Size(double price, String size){
         this.price = price;
+        this.size = size;
+    }
+
+    public String getSize(){
+        return size;
     }
 
     public double getPrice(){
