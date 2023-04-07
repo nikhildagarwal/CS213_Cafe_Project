58e2a6797c0cb2c58bc77d44c06a9ea97e352163
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-04-06 20:27:21 -0400

    orderBasketController comments

--- src/main/java/com/example/cs213_cafe_project/OrderBasketController.java
+++ src/main/java/com/example/cs213_cafe_project/OrderBasketController.java
@@ -1,3 +1,6 @@
+/**
+ * Project package
+ */
 package com.example.cs213_cafe_project;
 
 import com.example.cs213_cafe_project.data.BasketItem;
@@ -7,40 +10,79 @@ import com.example.cs213_cafe_project.donut.YeastDonut;
 import com.example.cs213_cafe_project.donut.flavors.YeastFlavor;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.paint.Color;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 
+/**
+ * Controller class for orderBasket.fxml
+ * Does all the event handling and the action handling for the orderBasket.
+ * Sends and receives data with the mainController.
+ * @author Nikhil Agarwal, Hyeon Oh
+ */
 public class OrderBasketController {
 
+    /**
+     * Variable to hold an instance of the mainController
+     */
     private MainController mainController;
 
+    /**
+     * The list of basketItems that can be viewed and interacted with in the GUI
+     */
     @FXML
     private ListView<BasketItem> basketListView;
+
+    /**
+     * The subtotal textField
+     */
     @FXML
     private TextField subTotal;
+
+    /**
+     * The tax amount textField
+     */
     @FXML
     private TextField tax;
+
+    /**
+     * The textField that holds the value of tax + subTotal
+     */
     @FXML
     private TextField total;
+
+    /**
+     * Remove button reference
+     */
     @FXML
     private Button removeButton;
+
+    /**
+     * placeOrderButton reference
+     */
     @FXML
     private Button placeOrderButton;
+
+    /**
+     * loadBasketButton button reference
+     */
     @FXML
     private Button loadBasketButton;
 
-    //Get the reference to the MainController object
+    /**
+     * Initializes an instance of the mainController and sets mainController to that value
+     * @param controller The controller class object
+     */
     public void setMainController (MainController controller){
         mainController = controller;
     }
 
+    /**
+     * init method that runs as soon as the .fxml file is set and staged
+     *      - set all monetary textFields to $0.00
+     *      - Disable all buttons until the load basket button is clicked to fetch data from mainController
+     */
     public void initialize(){
         subTotal.setText("$0.00");
         tax.setText("$0.00");
@@ -55,6 +97,12 @@ public class OrderBasketController {
         placeOrderButton.disableProperty().set(true);
     }
 
+    /**
+     * Loads all the basket items from the mainController data storage into our listView.
+     * The items can then be selected and some action carried out.
+     * Enables all the other functionalities.
+     * Sets the init values for the $ amounts in the textFields.
+     */
     @FXML
     public void setLoadBasket(){
         ObservableList<BasketItem> fullBasket = mainController.getFullBasket();
@@ -74,6 +122,10 @@ public class OrderBasketController {
         }
     }
 
+    /**
+     * Checks to see if the listView has been selected.
+     * If there is a value that has been selected, the remove button will be enabled.
+     */
     @FXML
     public void listViewSelected(){
         BasketItem selected = basketListView.getSelectionModel().getSelectedItem();
@@ -83,6 +135,10 @@ public class OrderBasketController {
         removeButton.disableProperty().set(false);
     }
 
+    /**
+     * event handler for the remove button
+     * Will remove the selected basketItem and update the prices that are displayed in the textFields.
+     */
     @FXML
     public void removeSelectedItem(){
         BasketItem selected = basketListView.getSelectionModel().getSelectedItem();
@@ -97,6 +153,10 @@ public class OrderBasketController {
         }
     }
 
+    /**
+     * Resets the entire page to what it was when initialized.
+     * Sends the data to the mainController as an order and will be added to the data storage of list of orders.
+     */
     @FXML
     public void placeOrder(){
         int orderNumber = createOrder();
@@ -109,6 +169,10 @@ public class OrderBasketController {
         loadBasketButton.disableProperty().set(false);
     }
 
+    /**
+     * Creates an order object based on the input fields.
+     * @return integer orderNumber associated with the order that was just created.
+     */
     private int createOrder(){
         ObservableList<BasketItem> fullBasket = mainController.getFullBasket();
         ArrayList<BasketItem> orderList = new ArrayList<>();
@@ -121,6 +185,11 @@ public class OrderBasketController {
         return orderNumber;
     }
 
+    /**
+     * Generates a random OrderNumber and checks that it is not currently in use.
+     * If the number is already in use it will keep creating orderNumbers until a unique number is found.
+     * @return integer order-number.
+     */
     private int generateOrderNumber(){
         HashSet<Integer> orderNumbers = mainController.getOrderNumbers();
         int randomNum = (int) (Math.random() * 1000);
@@ -132,6 +201,12 @@ public class OrderBasketController {
         }
     }
 
+    /**
+     * Creates the list of orders and sets the reference of this new object to the reference in the mainController data storage.
+     * Now this new object will be accessed to get a list of all the active orders.
+     * @param orderList The existing list of orders that we want to add our new order to
+     * @param fullBasket the list of basketItems that we are going to package into a new order.
+     */
     private void createOrderList(ArrayList<BasketItem> orderList,ObservableList<BasketItem> fullBasket){
         for(int i = 0;i<fullBasket.size();i++){
             BasketItem currBasketItem = fullBasket.get(i);
@@ -155,6 +230,11 @@ public class OrderBasketController {
         }
     }
 
+    /**
+     * Helper method to set the values of the textFields whenever there is some additive action (LOAD)
+     * or negative action (REMOVE)
+     * @param basket A list of all basket items that our method will loop through to calculate the price.
+     */
     private void setBasketPrice(ObservableList<BasketItem> basket){
         double subTotalAmount = 0.0;
         for(int i = 0;i< basket.size();i++){
@@ -171,6 +251,11 @@ public class OrderBasketController {
         total.setText(format(totalAmount));
     }
 
+    /**
+     * Helper method to format a double as a monetary amount
+     * @param amount double amount=
+     * @return String monetary value Ex: 7.58 --> "$7.58"
+     */
     private String format(double amount){
         String amountString = Double.toString(amount);
         String[] splitter = amountString.split("\\.");
